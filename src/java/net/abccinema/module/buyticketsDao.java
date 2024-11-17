46b718f4581a37bb0834a7564588737f8a50ee59
GavinRanasinghe29870 <gavinkisara@gmail.com>
2024-11-17 20:44:25 +0530

    Added buytickets.java & added buyticketsDao.java files to the net.abccinema.module package

---
+++ src/java/net/abccinema/module/buyticketsDao.java
@@ -0,0 +1,49 @@
+package net.abccinema.module;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author gavin
+ */
+public class buyticketsDao {
+
+    private Connection con;
+    private String query;
+    private PreparedStatement pst;
+    private ResultSet rs;
+
+    public buyticketsDao(Connection con) {
+        this.con = con;
+    }
+
+    public List<buytickets> getMovies() {
+        List<buytickets> movies = new ArrayList<>();
+
+        try {
+            query = "SELECT * from movies";
+            pst = this.con.prepareStatement(query);
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                buytickets row = new buytickets();
+                row.setId(rs.getInt("m_id"));
+                row.setName(rs.getString("m_name"));
+                row.setImage(rs.getString("m_image"));
+                row.setDescription(rs.getString("m_description"));
+                row.setTime(rs.getString("m_time"));
+                
+                movies.add(row);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return movies;
+    }
+}
