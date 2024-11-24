fc7fe73915fb860babcc058a0c4574915ffd9be5
agasthi23 <137294353+agasthi23@users.noreply.github.com>
2024-11-24 20:15:37 +0530

    Add Purchase Class with Fields and Getters/Setters purchase.java

--- src/java/net/abccinema/model/purchase.java
+++ src/java/net/abccinema/model/purchase.java
@@ -5,9 +5,99 @@
 package net.abccinema.model;
 
 /**
- *
- * @author ASUS
+ * Model class for purchase details.
  */
 public class purchase {
-    
+
+    private int id;            
+    private String name;       
+    private String phoneNumber; 
+    private String email;       
+    private int adultTickets;  
+    private int childTickets;   
+    private double totalAmount; 
+    private String paymentMethod; 
+
+    // Constructor
+    public purchase() {}
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAdultTickets() {
+        return adultTickets;
+    }
+
+    public void setAdultTickets(int adultTickets) {
+        this.adultTickets = adultTickets;
+    }
+
+    public int getChildTickets() {
+        return childTickets;
+    }
+
+    public void setChildTickets(int childTickets) {
+        this.childTickets = childTickets;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", adultTickets=" + adultTickets +
+                ", childTickets=" + childTickets +
+                ", totalAmount=" + totalAmount +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                '}';
+    }
 }
