ae08648b3346ed8c626d98e3980f2d9f9db603e1
VihangaDewindi <137777722+VihangaDewindi@users.noreply.github.com>
2024-11-30 20:59:25 +0530

    Added single movie DAO

---
+++ src/java/net/abccinema/model/singleMovieDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.abccinema.model;
+
+import net.abccinema.connection.DbCon;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+/**
+ *
+ * @author Vihanga
+ */
+public class singleMovieDAO {
+    private static final String URL = "jdbc:mysql://localhost:3306/abccinema";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+}
